4fad047ecd281778457ec4b2aabbacf1dafdc122
Anna Volodko <anyavolodko95@gmail.com>
2023-10-26 00:08:18 +0300

    :(

--- Project 2/js/form.js
+++ Project 2/js/form.js
@@ -1,16 +1,16 @@
-const EMAIL_MIN_LENGTH = 5;
+// const EMAIL_MIN_LENGTH = 5;
 
-function checkEmailLenght() {
-    const valueLenght = window.inputEmail.value.length;
-    const diff = valueLenght < EMAIL_MIN_LENGTH ? EMAIL_MIN_LENGTH - valueLenght : 0;
+// function checkEmailLenght() {
+//     const valueLenght = window.inputEmail.value.length;
+//     const diff = valueLenght < EMAIL_MIN_LENGTH ? EMAIL_MIN_LENGTH - valueLenght : 0;
 
-    if(diff) {
-        window.emailDiffCount.textContent = diff;
-        window.emailLenghtHelp.classList.remove('d-none');
-    } else {
-        window.emailLenghtHelp.classList.add('d-none');
-    }
-};
+//     if(diff) {
+//         window.emailDiffCount.textContent = diff;
+//         window.emailLenghtHelp.classList.remove('d-none');
+//     } else {
+//         window.emailLenghtHelp.classList.add('d-none');
+//     }
+// };
 
-  window.inputEmail.addEventListener('input', checkEmailLenght)
-  document.addEventListener('DOMContentLoaded', checkEmailLenght)
+//   window.inputEmail.addEventListener('input', checkEmailLenght)
+//   document.addEventListener('DOMContentLoaded', checkEmailLenght)
